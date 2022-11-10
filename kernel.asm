15f2c96db9dfec410bab72d4adf8194c1901406d
Nathan Dygant <nathan.dygant@gmail.com>
2022-11-10 14:46:46 +0100

    Adding interruption and ring 3

--- kernel.asm
+++ kernel.asm
@@ -2,7 +2,32 @@
 [ORG 0x200000]
 
 start:
+    mov rdi,Idt
+    mov rax,Handler0
+    call SetHandler
+
+    mov rax,Timer
+    mov rdi,Idt+32*16
+    call SetHandler
+
+    mov rdi,Idt+32*16+7*16
+    mov rax,SIRQ
+    call SetHandler
+    
     lgdt [Gdt64Ptr]
+    lidt [IdtPtr]
+
+SetTss:
+    mov rax,Tss
+    mov [TssDesc+2],ax
+    shr rax,16
+    mov [TssDesc+4],al
+    shr rax,8
+    mov [TssDesc+7],al
+    shr rax,8
+    mov [TssDesc+8],eax
+    mov ax,0x20
+    ltr ax
 
     push 8
     push KernelEntry
@@ -13,15 +38,233 @@ KernelEntry:
     mov byte[0xb8000],'K'
     mov byte[0xb8001],0xa
 
+InitPIT:
+    mov al,(1<<2)|(3<<4)
+    out 0x43,al
+
+    mov ax,11931
+    out 0x40,al
+    mov al,ah
+    out 0x40,al
+
+InitPIC:
+    mov al,0x11
+    out 0x20,al
+    out 0xa0,al
+
+    mov al,32
+    out 0x21,al
+    mov al,40
+    out 0xa1,al
+
+    mov al,4
+    out 0x21,al
+    mov al,2
+    out 0xa1,al
+
+    mov al,1
+    out 0x21,al
+    out 0xa1,al
+
+    mov al,11111110b
+    out 0x21,al
+    mov al,11111111b
+    out 0xa1,al
+
+    push 0x18|3
+    push 0x7c00
+    push 0x202
+    push 0x10|3
+    push UserEntry
+    iretq
+
 End:
     hlt
     jmp End
 
+SetHandler:
+    mov [rdi],ax
+    shr rax,16
+    mov [rdi+6],ax
+    shr rax,16
+    mov [rdi+8],eax
+    ret
+
+UserEntry:
+    inc byte[0xb8010]
+    mov byte[0xb8011],0xF
+    jmp UserEntry
+
+Handler0:
+    push rax
+    push rbx  
+    push rcx
+    push rdx  	  
+    push rsi
+    push rdi
+    push rbp
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+    
+    mov byte[0xb8000],'D'
+    mov byte[0xb8001],0xc
+
+    jmp End
+
+    pop	r15
+    pop	r14
+    pop	r13
+    pop	r12
+    pop	r11
+    pop	r10
+    pop	r9
+    pop	r8
+    pop	rbp
+    pop	rdi
+    pop	rsi  
+    pop	rdx
+    pop	rcx
+    pop	rbx
+    pop	rax
+
+    iretq
+
+Timer:
+    push rax
+    push rbx  
+    push rcx
+    push rdx  	  
+    push rsi
+    push rdi
+    push rbp
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+
+    inc byte[0xb8020]
+    mov byte[0xb8021],0xe
+    
+    mov al,0x20
+    out 0x20,al
+   
+    pop	r15
+    pop	r14
+    pop	r13
+    pop	r12
+    pop	r11
+    pop	r10
+    pop	r9
+    pop	r8
+    pop	rbp
+    pop	rdi
+    pop	rsi  
+    pop	rdx
+    pop	rcx
+    pop	rbx
+    pop	rax
+
+    iretq
+
+SIRQ:
+    push rax
+    push rbx  
+    push rcx
+    push rdx  	  
+    push rsi
+    push rdi
+    push rbp
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+
+    mov al,11
+    out 0x20,al
+    in al,0x20
+
+    test al,(1<<7)
+    jz .end
+
+    mov al,0x20
+    out 0x20,al
+
+.end:
+    pop	r15
+    pop	r14
+    pop	r13
+    pop	r12
+    pop	r11
+    pop	r10
+    pop	r9
+    pop	r8
+    pop	rbp
+    pop	rdi
+    pop	rsi  
+    pop	rdx
+    pop	rcx
+    pop	rbx
+    pop	rax
+
+    iretq
+
+
 Gdt64:
     dq 0
     dq 0x0020980000000000
+    dq 0x0020f80000000000
+    dq 0x0000f20000000000
+TssDesc:
+    dw TssLen-1
+    dw 0
+    db 0
+    db 0x89
+    db 0
+    db 0
+    dq 0
+
 
 Gdt64Len: equ $-Gdt64
 
+
 Gdt64Ptr: dw Gdt64Len-1
           dq Gdt64
+
+
+Idt:
+    %rep 256
+        dw 0
+        dw 0x8
+        db 0
+        db 0x8e
+        dw 0
+        dd 0
+        dd 0
+    %endrep
+
+IdtLen: equ $-Idt
+
+IdtPtr: dw IdtLen-1
+        dq Idt
+
+Tss:
+    dd 0
+    dq 0x150000
+    times 88 db 0
+    dd TssLen
+
+TssLen: equ $-Tss
